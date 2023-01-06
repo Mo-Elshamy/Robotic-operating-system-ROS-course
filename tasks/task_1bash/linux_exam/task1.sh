c3d15845fa8552bb9e4c09744c9ec8292c6660e1
Mo_Elshamy <moelshamy742@gmail.com>
2023-01-06 19:06:04 +0200

    task 1 bash submition

---
+++ tasks/task_1bash/linux_exam/task1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/Desktop/linx_exam
+mkdir -v -p this/is/my/linux/exam
+cd this/is/my/linux/exam
+touch my_file.py
+echo 'This bash script has finished'
---
+++ tasks/task_1bash/linux_exam/task3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd this/is/my/linux/exam
+rm -v *
+touch exam1.py exam2.py exam3.py
+chmod 754 exam1.py
+chmod 501 exam2.py
+chmod 241 exam3.py
+
